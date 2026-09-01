3d8b1a6e3ad587105722d8d8324924b6297193c7
ibrahim5126 <128904295+ibrahim5126@users.noreply.github.com>
2026-09-01 21:26:30 +0500

    halfway through libraries

--- month-01-python-fundamentals/practice/average.py
+++ month-01-python-fundamentals/practice/average.py
@@ -1,3 +1,5 @@
 import statistics
 
-print(statistics.mean([20, 50, 30, 40, 89]))
+print("Mean = ", statistics.mean([20, 50, 30, 40, 89]))
+print("Median = ", statistics.median([3, 90, 2, 56, 100]))
+print("Mode = ", statistics.mode([1, 1, 2, 3, 4,4,4,4,5,8]))
